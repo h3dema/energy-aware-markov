dbec1f20228962190fcd0ecd12c148d9cc65902b
henrique moura <h3dema@users.noreply.github.com>
2025-07-02 16:12:06 +0200

    docstring

--- state.h
+++ state.h
@@ -1,14 +1,16 @@
 #ifndef __STATE_H__
 #define __STATE_H__
 
-enum class State { ACTIVE, INACTIVE };
+enum class State { INACTIVE, ACTIVE };  // keep this order, so ACTIVE = 1
 
 
 class EnergyStateMachine {
 private:
-    State state;
-    double delta0;
-    double delta1;
+    double delta0;  // transition probability from INACTIVE to ACTIVE
+    double delta1;  // transition probability from ACTIVE to INACTIVE
+
+    State state;  // active or inactive
+
     std::mt19937 generator;
     std::uniform_real_distribution<double> distribution;
 
@@ -16,9 +18,12 @@ public:
 
     EnergyStateMachine(double d0, double d1);
     State current_state() const;
+    
     void set_state(State new_state);
-    State step();
+    void set_delta0(double d0);
+    void set_delta1(double d1);
 
+    State step();
 };
 
 
